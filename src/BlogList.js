0a6a4a8a2241289d39ee24f1464ab5f2e117f40d
SebbieMzingKe <sebbievilar2@gmail.com>
2024-12-19 21:38:39 +0300

    Update (chore): modify bloglist

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,26 +1,49 @@
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
-const BlogList = ({blogs, title}) => {
-    // const blogs = props.blogs
-    // const title = props.title
-
-
+const BlogList = ({ blogs, title }) => {
     return ( 
         <div className="blog-list">
             <h2>{title}</h2>
         
-            {blogs?.map((blog) => (
-                <div className="blog-previews" key = {blog.id}>
-                    <Link to = {`/blogs/${blog.id}`}>
-                    <h2>{blog.title}</h2>
-                    <p>written by {blog.author}</p>
+            {Array.isArray(blogs) && blogs.map((blog) => (
+                <div className="blog-previews" key={blog.id}>
+                    <Link to={`/blogs/${blog.id}`}>
+                        <h2>{blog.title}</h2>
+                        <p>written by {blog.author}</p>
                     </Link>
-                    
-                    
                 </div>
             ))}
         </div>
-     );
+    );
 }
  
 export default BlogList;
+
+
+
+// import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
+// const BlogList = ({blogs, title}) => {
+//     // const blogs = props.blogs
+//     // const title = props.title
+
+
+//     return ( 
+//         <div className="blog-list">
+//             <h2>{title}</h2>
+        
+//             {blogs?.map((blog) => (
+//                 <div className="blog-previews" key = {blog.id}>
+//                     <Link to = {`/blogs/${blog.id}`}>
+//                     <h2>{blog.title}</h2>
+//                     <p>written by {blog.author}</p>
+//                     </Link>
+                    
+                    
+//                 </div>
+//             ))}
+//         </div>
+//      );
+// }
+ 
+// export default BlogList;
