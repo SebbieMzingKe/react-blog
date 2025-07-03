4c01f8491e5db885fdddce1cfe2c7e4685eabda6
Sebbie Evayo <115040489+SebbieMzingKe@users.noreply.github.com>
2025-07-03 12:32:57 +0300

    Updated app-build-manifest.json

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("7ae4f7d3e5848d23")
+/******/ 		__webpack_require__.h = () => ("4e268cd6466f0d59")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8ca6bc264ca231d7"; }
+/******/ 		__webpack_require__.h = function() { return "740a36894429beab"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
